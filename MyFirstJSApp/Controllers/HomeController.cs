d0ed4160c2466aa7ae95edd36c1dda07057df5d0
kadirmuratbaseren <kadirmuratbaseren@gmail.com>
2016-09-07 10:02:26 +0300

    Add project files.

---
+++ MyFirstJSApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstJSApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult Index3()
+        {
+            return View();
+        }
+
+        public ActionResult Index4()
+        {
+            return View();
+        }
+
+        public ActionResult Index5()
+        {
+            return View();
+        }
+
+        public ActionResult Index6()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFirstJSApp/Scripts/merhaba.js
@@ -0,0 +1,4 @@
+﻿function MerahabaDe2()
+{
+    alert("Merhaba Dünya");
+}
